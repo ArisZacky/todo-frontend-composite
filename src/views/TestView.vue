3cf3f3510d467dc9fec4e6b77ec7595d872a0ef0
unknown <rizkyariszacky@gmail.com>
2024-02-25 21:58:43 +0800

    add form component

--- src/views/TestView.vue
+++ src/views/TestView.vue
@@ -2,10 +2,27 @@
     import { ref } from 'vue'
     import { useListStore } from '@/stores/lists'
 
+    // store container
     const store = useListStore()
 
+    // initial input / default input
+    const defaultInput = {
+        name: '',
+        hobby: ''
+    }
+
     // ref input
-    const nameInput = ref('')
+    // spread syntax
+    const input = ref({ ...defaultInput })
+
+    // function yang menerima submit form
+    function onSubmit(){
+        // event.preventDefault();
+        console.log({ ...input.value })
+
+        // add list via store
+        store.addList({ ...input.value })
+    }
 </script>
 
 <template>
@@ -15,14 +32,18 @@
         <!-- add v-model to integrate data binding with ref -->
         <!-- add event handler listener when keyup enter -->
         <!-- method handler with addList function -->
-        <!-- event modifier .enter -->
-        <input type="text"
-        v-model="nameInput"
-        name="name"
-        @keyup.enter="store.addList(nameInput); nameInput = ''">
+        <!-- event modifier .enter, .prevent -->
+        <form class="form" @submit.prevent="onSubmit">
+            <input type="text" class="input" v-model="input.name" name="name" placeholder="John">
+            <input type="text" class="input" v-model="input.hobby" name="hobby" placeholder="Gaming">
+            <button type="submit">Submit</button>
+        </form>
+
         <ol>
-            <template v-for="item in store.getList" v-bind:key="item">
-                <li>{{ item.name }}</li>
+            <!-- (item, index) -->
+
+            <template v-for="(item, index) in store.getList" v-bind:key="index">
+                <li class="underline">{{ item.name }} ({{ item.hobby }}) - {{ index }}</li>
             </template>
         </ol>
     </div>
@@ -32,8 +53,13 @@
 <!-- scoped untuk melimitasi hanya di komponen -->
 <style scoped lang="scss">
     /* body = font-size: 16px (1rem) */
-    .input {
-        /* 2rem = 16px * 2 */
+    .form {
+        .input {
+            /* 2rem = 16px * 2 */
+            margin-block-end: 1rem;
+            width: 100%;
+        }
+
         margin-block-end: 2rem;
     }
     .list {
