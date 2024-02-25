9af34183a101354a07eae4caaecab93163b085b4
unknown <rizkyariszacky@gmail.com>
2024-02-25 22:20:47 +0800

    move input to new component

--- src/components/BaseInput.vue
+++ src/components/BaseInput.vue
@@ -1,3 +1,43 @@
+<script setup>
+    // komunikasi dari parent ke children; props
+    defineProps({
+        id: {
+            type: String,
+            default: 'id'
+        },
+        name:{
+            type: String,
+            default: 'name'
+        },
+        placeholder: {
+            type: String,
+            default: 'Placeholder'
+        },
+        modelValue: {
+            type: String,
+            default: ''
+        }
+    })
+    // Komunikasi dari children ke parent; emit
+    defineEmits(['update:modelValue'])
+</script>
+
 <template>
-    <input type="text">
+    <label for="id">{{ name }}</label>
+    <input 
+    :id="id"
+    class="input"
+    type="text"
+    :name="name"
+    :placeholder="placeholder"
+    :value="modelValue"
+    @input="(e) => $emit('update:modelValue', e.target.value)">
 </template>
+
+<style scoped>
+    .input {
+        /* 2rem = 16px * 2 */
+        margin-block-end: 1 rem;
+        width: 100%;
+    }
+</style>
